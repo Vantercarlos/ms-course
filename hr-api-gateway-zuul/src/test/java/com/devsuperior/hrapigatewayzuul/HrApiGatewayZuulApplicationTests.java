126042d90b3d4d095e6aaa3bc6092c90d29720fb
vantercarlos <vantercarlos.caetano@cs.inf.br>
2021-07-07 00:11:48 -0400

    Configure zuul api-gateway

--- hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devsuperior.hrapigetewayzuul;
+package com.devsuperior.hrapigatewayzuul;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrApiGetewayZuulApplicationTests {
+class HrApiGatewayZuulApplicationTests {
 
 	@Test
 	void contextLoads() {
